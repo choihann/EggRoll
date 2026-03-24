71d952547865ea990d16067bd0db380bea08043b
Hanna Choi <choihan006@gmail.com>
2026-03-24 00:12:01 -0600

    fixed dog factory

--- src/main/java/eggroll/pet/DogFactory.java
+++ src/main/java/eggroll/pet/DogFactory.java
@@ -3,42 +3,35 @@ package eggroll.pet;
 import java.util.List;
 import java.util.Random;
 
-public class DogFactory {
-    package eggroll.pet;
+public class DogFactory extends PetFactory {
+    private final Random random = new Random();
+    private static final List<String> COMMON_SPECIES = List.of("DOG");
+    private static final List<String> RARE_SPECIES = List.of("DOG"); // replace with rare pets when added
+    private static final List<String> EPIC_SPECIES = List.of("DOG"); // replace this as well
 
-import java.util.List;
-import java.util.Random;
+    @Override
+    public Pet createPet(PetRarity rarity) {
+        String petSpecies = pickSpecies(rarity);
+        return makeDog(petSpecies, rarity);
+    }
 
-    public class CatFactory extends PetFactory{
-        private final Random random = new Random();
-        private static final List<String> COMMON_SPECIES = List.of("DOG");
-        private static final List<String> RARE_SPECIES = List.of("DOG"); // replace with rare pets when added
-        private static final List<String> EPIC_SPECIES = List.of("DOG"); // replace this as well
+    // helpers
+    private String pickSpecies(PetRarity rarity) {
+        List<String> speciesPool = switch (rarity) {
+            case Rare -> RARE_SPECIES;
+            case Epic -> EPIC_SPECIES;
+            default -> COMMON_SPECIES;
+        };
+        return speciesPool.get(random.nextInt(speciesPool.size()));
+    }
 
-        @Override
-        public Pet createPet(PetRarity rarity) {
-            String petSpecies = pickSpecies(rarity);
-            return makeDog(petSpecies, rarity);
-        }
-
-        // helpers
-        private String pickSpecies(PetRarity rarity) {
-            List<String> speciesPool = switch (rarity) {
-                case Rare -> RARE_SPECIES;
-                case Epic -> EPIC_SPECIES;
-                default -> COMMON_SPECIES;
-            };
-            return speciesPool.get(random.nextInt(speciesPool.size()));
-        }
-
-        private Pet makeDog(String species, PetRarity rarity) {
-            return switch (species.toLowerCase()) {
-                case "dog" -> new Dog();
-                default -> new Dog(); // populate this once we have more species of cat
-            };
-
-        }
+    private Pet makeDog(String species, PetRarity rarity) {
+        return switch (species.toLowerCase()) {
+            case "dog" -> new Dog();
+            default -> new Dog(); // populate this once we have more species of cat
+        };
 
     }
 
 }
+
