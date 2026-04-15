fb58fad2c4c286b2f7c788cbd7da36c64c63650c
KieuNhiL <luleirbag@gmail.com>
2026-04-14 20:04:02 -0600

    Some better names, tests for queuedState, potential interface for evolveStrat

---
+++ src/test/java/eggroll/pet/PetTest.java
@@ -0,0 +1,90 @@
+package eggroll.pet;
+
+import eggroll.pet.petstate.PetState;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PetTest {
+    Pet animalPet;
+
+    @BeforeEach
+    void setUp() {
+        animalPet = new Cat();
+    }
+
+    @Test
+    void testIsEgg() {
+        assertTrue(animalPet.isEgg);
+    }
+
+    @Test
+    void testSpawnsInUnbornState() {
+        assertEquals(animalPet.getUnbornState(), animalPet.getCurrentState());
+    }
+
+    @Test
+    void testIncreaseStatActuallyIncreasesStat(){
+        int initialHappiness = animalPet.getHappinessStat();
+        animalPet.increaseStat(1,"Happiness");
+        assertEquals(initialHappiness + 1, animalPet.getHappinessStat());
+    }
+
+    @Test
+    void testDecreaseStatActuallyDecreasesStat(){
+        int initialHappiness = animalPet.getHappinessStat();
+        animalPet.decreaseStat(1, "Happiness");
+        assertEquals(initialHappiness - 1, animalPet.getHappinessStat());
+    }
+
+    @Test
+    void testIsStatMax(){
+        animalPet.setStat(5, "happiness");
+        assertTrue(animalPet.isStatMax());
+    }
+
+    @Test
+    void testQueueingStatesActuallyQueuesStatesToThePet(){
+        animalPet.pushToQueuedStates(animalPet.getUnfitState());
+        animalPet.pushToQueuedStates(animalPet.getDirtyState());
+        animalPet.pushToQueuedStates(animalPet.getTiredState());
+        animalPet.pushToQueuedStates(animalPet.getSadState());
+        animalPet.pushToQueuedStates(animalPet.getHungryState());
+        animalPet.pushToQueuedStates(animalPet.getNormalState());
+
+        Queue<PetState> expectedStates = new LinkedList<>();
+        expectedStates.add(animalPet.getHungryState());
+        expectedStates.add(animalPet.getDirtyState());
+        expectedStates.add(animalPet.getTiredState());
+        expectedStates.add(animalPet.getSadState());
+        expectedStates.add(animalPet.getUnfitState());
+        expectedStates.add(animalPet.getNormalState());
+
+        assertTrue(animalPet.getQueuedStates().containsAll(expectedStates));
+    }
+
+    @Test
+    void testPollingQueueStatesActuallyPolls(){
+        animalPet.pushToQueuedStates(animalPet.getNormalState());
+        animalPet.setCurrentState(animalPet.popOffQueuedState());
+
+        assertEquals(animalPet.getNormalState(), animalPet.getCurrentState());
+    }
+
+    @Test
+    void testCheckIfNeedsPenaltyReturnsProperly(){
+        assertEquals(false, animalPet.canEvolve());
+
+        animalPet.setCurrentState(animalPet.getNormalState());
+        animalPet.setStat(5, "happiness");
+        animalPet.setIsEgg(false);
+
+        assertEquals(true, animalPet.canEvolve());
+
+    }
+}
