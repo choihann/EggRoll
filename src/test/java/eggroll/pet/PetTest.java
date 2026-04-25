7ffb90f84db6888d3dc927fba8a1282689ec6c17
KieuNhiL <luleirbag@gmail.com>
2026-04-25 15:22:22 -0600

    Divided pet testing into appropriate test files

--- src/test/java/eggroll/pet/PetTest.java
+++ src/test/java/eggroll/pet/PetTest.java
@@ -29,11 +29,6 @@ public class PetTest {
     }
 
     @Test
-    void testSpawnsInUnbornState() {
-        assertEquals(animalPet.getUnbornState(), animalPet.getCurrentState());
-    }
-
-    @Test
     void testIncreaseStatActuallyIncreasesStat(){
         int initialHappiness = animalPet.getHappinessStat();
         animalPet.increaseStat(1, PetStatType.HAPPINESS);
@@ -75,135 +70,6 @@ public class PetTest {
         assertTrue(animalPet.isStatMax());
     }
 
-
-    @Test
-    void testCheckIfNeedsPenaltyReturnsProperly(){
-        assertEquals(false, animalPet.canEvolve());
-
-        animalPet.setCurrentState(animalPet.getNormalState());
-        animalPet.setStat(5, PetStatType.HAPPINESS);
-        animalPet.setIsEgg(false);
-
-        assertEquals(true, animalPet.canEvolve());
-
-    }
-
-    @Test
-    void testPetInHungryStateWontBathe(){
-        EvolutionStrategy juvenileEvo = animalPet.strategyFactory.newJuvenileStrategy();
-        animalPet.setCurrentStrategy(juvenileEvo);
-        animalPet.setCurrentState(animalPet.getHungryState());
-        int hygieneBefore = animalPet.getHygieneStat();
-
-        animalPet.bathe();
-        assertTrue(animalPet.getHygieneStat() == hygieneBefore);
-    }
-
-    @Test
-    void testPetInHungryStateWontHaveFullnessAsAPenalizableStat(){
-        EvolutionStrategy juvenileEvo = animalPet.strategyFactory.newJuvenileStrategy();
-        animalPet.setCurrentStrategy(juvenileEvo);
-        animalPet.setCurrentState(animalPet.getHungryState());
-        Set<PetStatType> thePenalizableStats = animalPet.getPenalizableStats();
-
-        assertFalse(thePenalizableStats.contains(PetStatType.FULLNESS));
-        assertTrue(animalPet.currentState.canEat());
-        assertFalse(animalPet.currentState.canBathe());
-        assertFalse(animalPet.currentState.canPlay());
-        assertFalse(animalPet.currentState.canNap());
-    }
-
-    @Test
-    void testPetInDirtyStateReturnsProperCommandPermissions(){
-        EvolutionStrategy juvenileEvo = animalPet.strategyFactory.newJuvenileStrategy();
-        animalPet.setCurrentStrategy(juvenileEvo);
-        animalPet.setCurrentState(animalPet.getDirtyState());
-        Set<PetStatType> thePenalizableStats = animalPet.getPenalizableStats();
-
-        assertFalse(thePenalizableStats.contains(PetStatType.HYGIENE));
-        assertTrue(animalPet.currentState.canBathe());
-        assertFalse(animalPet.currentState.canEat());
-        assertFalse(animalPet.currentState.canPlay());
-        assertFalse(animalPet.currentState.canNap());
-    }
-
-    @Test
-    void testPetInNormalStateReturnsProperCommandPermissions(){
-        EvolutionStrategy juvenileEvo = animalPet.strategyFactory.newJuvenileStrategy();
-        animalPet.setCurrentStrategy(juvenileEvo);
-        animalPet.setCurrentState(animalPet.getNormalState());
-        Set<PetStatType> thePenalizableStats = animalPet.getPenalizableStats();
-
-        assertTrue(thePenalizableStats.contains(PetStatType.FULLNESS));
-        assertTrue(thePenalizableStats.contains(PetStatType.ENERGY));
-        assertTrue(thePenalizableStats.contains(PetStatType.HYGIENE));
-        assertTrue(thePenalizableStats.contains(PetStatType.HAPPINESS));
-
-        assertTrue(animalPet.currentState.canBathe());
-        assertTrue(animalPet.currentState.canEat());
-        assertTrue(animalPet.currentState.canPlay());
-        assertTrue(animalPet.currentState.canNap());
-    }
-
-    @Test
-    void testPetInTiredStateReturnsProperCommandPermissions(){
-        EvolutionStrategy juvenileEvo = animalPet.strategyFactory.newJuvenileStrategy();
-        animalPet.setCurrentStrategy(juvenileEvo);
-        animalPet.setCurrentState(animalPet.getTiredState());
-        Set<PetStatType> thePenalizableStats = animalPet.getPenalizableStats();
-
-        assertFalse(thePenalizableStats.contains(PetStatType.ENERGY));
-        assertTrue(animalPet.currentState.canNap());
-        assertFalse(animalPet.currentState.canEat());
-        assertFalse(animalPet.currentState.canPlay());
-        assertFalse(animalPet.currentState.canBathe());
-    }
-
-    @Test
-    void testPetInUnbornStateReturnsProperCommandPermissions(){
-        EvolutionStrategy unbornEvo = animalPet.strategyFactory.newUnbornStrategy();
-        animalPet.setCurrentStrategy(unbornEvo);
-        animalPet.setCurrentState(animalPet.getUnbornState());
-        Set<PetStatType> thePenalizableStats = animalPet.getPenalizableStats();
-
-        assertFalse(thePenalizableStats.contains(PetStatType.FULLNESS));
-        assertFalse(thePenalizableStats.contains(PetStatType.ENERGY));
-        assertFalse(thePenalizableStats.contains(PetStatType.HYGIENE));
-        assertFalse(thePenalizableStats.contains(PetStatType.HAPPINESS));
-
-        assertFalse(animalPet.currentState.canBathe());
-        assertFalse(animalPet.currentState.canEat());
-        assertTrue(animalPet.currentState.canPlay());
-        assertFalse(animalPet.currentState.canNap());
-    }
-
-    @Test
-    void testAdultPetsCannotEvolve(){
-        EvolutionStrategy adultEvo = animalPet.strategyFactory.newAdultStrategy();
-        animalPet.setCurrentStrategy(adultEvo);
-        animalPet.setCurrentState(animalPet.getNormalState());
-
-        assertFalse(animalPet.canEvolve());
-    }
-
-    @Test
-    void testJuvenilePetsCanEvolveWhenHappinessMax(){
-        EvolutionStrategy juvenileEvo = animalPet.strategyFactory.newJuvenileStrategy();
-        animalPet.setCurrentStrategy(juvenileEvo);
-        animalPet.setCurrentState(animalPet.getNormalState());
-        animalPet.setStat(7, PetStatType.HAPPINESS);
-
-        assertTrue(animalPet.canEvolve());
-    }
-
-    @Test
-    void testJuvenilePetsCannotEvolveWhenHappinessNotMax(){
-        EvolutionStrategy juvenileEvo = animalPet.strategyFactory.newJuvenileStrategy();
-        animalPet.setCurrentStrategy(juvenileEvo);
-        animalPet.setCurrentState(animalPet.getNormalState());
-
-        assertFalse(animalPet.canEvolve());
-    }
 
     @Test
     void testLowerRandomStatDoesNothingWithEmptyPenalizableStats() {
@@ -318,23 +184,6 @@ public class PetTest {
     }
 
     @Test
-    void evolveOnceEggMovesPetToAppropriateStrategy() {
-        animalPet.setStat(5, PetStatType.HAPPINESS);
-        animalPet.evolve();
-        assertEquals(EvolutionStage.JUVENILE, animalPet.getEvolutionStage());
-    }
-
-    @Test
-    void evolveTwiceEggMovesPetToAppropriateStrategy() {
-        animalPet.setStat(5, PetStatType.HAPPINESS);
-        animalPet.evolve();
-
-        animalPet.setStat(7, PetStatType.HAPPINESS);
-        animalPet.evolve();
-        assertEquals(EvolutionStage.ADULT, animalPet.getEvolutionStage());
-    }
-
-    @Test
     void testDetermineNextStateReturnsUnbornStateWhenPetIsStillEgg() {
         EvolutionStrategy juvenileEvo = animalPet.strategyFactory.newJuvenileStrategy();
         animalPet.setCurrentStrategy(juvenileEvo);
@@ -420,57 +269,6 @@ public class PetTest {
         assertEquals(animalPet.getTiredState(), animalPet.determineNextState());
     }
 
-    @Test
-    void testNapCommandIncreasesEnergy() {
-        animalPet.setIsEgg(false);
-        animalPet.setCurrentState(animalPet.getNormalState());
-
-        int initialEnergy = animalPet.getEnergyStat();
-
-        Command napCommand = new NapCommand(animalPet);
-        napCommand.execute();
-
-        assertEquals(initialEnergy + 1, animalPet.getEnergyStat());
-    }
-
-    @Test
-    void testPlayCommandIncreasesHappiness() {
-        animalPet.setIsEgg(false);
-        animalPet.setCurrentState(animalPet.getNormalState());
-
-        int initialHappiness = animalPet.getHappinessStat();
-
-        Command playCommand = new PlayCommand(animalPet);
-        playCommand.execute();
-
-        assertEquals(initialHappiness + 1, animalPet.getHappinessStat());
-    }
-
-    @Test
-    void testBatheCommandIncreasesHygiene() {
-        animalPet.setIsEgg(false);
-        animalPet.setCurrentState(animalPet.getNormalState());
-
-        int initialHygiene = animalPet.getHygieneStat();
-
-        Command batheCommand = new BatheCommand(animalPet);
-        batheCommand.execute();
-
-        assertEquals(initialHygiene + 1, animalPet.getHygieneStat());
-    }
-
-    @Test
-    void testFeedCommandIncreasesFullness() {
-        animalPet.setIsEgg(false);
-        animalPet.setCurrentState(animalPet.getNormalState());
-
-        int initialFullness = animalPet.getFullnessStat();
-
-        Command feedCommand = new FeedCommand(animalPet);
-        feedCommand.execute();
-
-        assertEquals(initialFullness + 1, animalPet.getFullnessStat());
-    }
 }
 
 
